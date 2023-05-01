64f4dee6ecb7a4f1cc42df1db33b5a1edc77eed6
HariHaran <jphhariharan0510@gmail.com>
2023-04-30 21:49:00 -0400

    Updated

--- bulid.sh
+++ bulid.sh
@@ -6,13 +6,18 @@ python3 -m pip install -r requirements.txt -t  trigger_sns/packages/
 
 ls -lrt trigger_sns/packages/
 
+pwd
+
 cd trigger_sns/
 
 zip -rq $lambda_function_name lambda_function.py
 
 cd packages/
 
+
 ls -lrt
+
+pwd
 
 zip -rq /trigger_sns/packages/$lambda_function_name .
 
